ae27bdf722e74ff2d1f89b20abe4df13eb7b961a
LeonardoBoratti <91642976+LeonardoBoratti@users.noreply.github.com>
2021-11-29 21:42:23 -0300

    Trabalho_poo

---
+++ Jovem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaDiscurciva
+{ 
+    class Jovem   
+    {
+
+        public string nome;
+        public string sexo;
+        public string idade;
+        public string situacao;
+        public void HomensTrab()
+        {
+            nome = "\n1-Luiz Alberto de Souza \n2-Vitor Silva\n3-João Vargas\n4-Teodoro Veiga\n5-Antonio Santos\n6-Jose Da Silva\n7-Francisco Alves" +
+                      "\n8-Igor Sanches\n9-Inacio Oliveira\n10-João scarpa\n11-Afonso Padilha\n12-Argeu Eccel\n13-Jaison Gili\n14-Rafael Ramos\n15-Matheus Garcia" +
+                      "\n16-Leandro Montes\n17-Evandro Silva\n18-Robson Alexandre\n19-Leonardo Boratti ";
+            sexo = "Masculinos";
+            idade = "16 à 54 Anos";
+            situacao = "Trabalhando";
+            Console.WriteLine("Cadastro dos Homens que trabalham!!! ");
+            Console.WriteLine("Nomes:" + nome + "\nSexo:" + sexo + "\nIdade: " + idade + "\nGenero: " + situacao + "\n");
+        }
+        public void HomensNaoTrab()
+        {
+            nome = "\n1-Junior Ramos\n2-Willian Gambeta\n3-Talles Alves\n4-Timoteo Tavares\n5-Celio Silva" +
+                "\n6-Sinesio Boratti\n7-João Bacca\n8-Vanderlei Marques\n9-Algusto Durkop";
+            idade = "16 à 32 Anos";
+            situacao = "Não Trabalha";
+            Console.WriteLine("Cadastro dos Homens que NÃO Trabalham!!! ");
+            Console.WriteLine("Nomes:" + nome + "\nSexo:" + sexo + "\nIdade: " + idade + "\nGenero: " + situacao + "\n");
+
+        }
+        public void MulhesTrab()
+        {
+            nome = "\n1-Albertina de Souza \n2-Stefany Silva\n3-Jessica Vargas\n4-Tatiana Santos\n5-Amelia Raitz\n6-Josiane Ramos\n7-Francisca Albino" +
+                      "\n8-Maria das Dores";
+            idade = "16 à 51 Anos";
+            situacao = "Trabalhando";
+            Console.WriteLine("Cadastro dos Mulheres que trabalham!!! ");
+            Console.WriteLine("Nomes:" + nome + "\nSexo:" + sexo + "\nIdade: " + idade + "\nGenero: " + situacao + "\n");
+        }
+        public void MulhesNaoTrab()
+        {
+            nome = "\n1-Jurema Gatis\n2-Priscila Gambeta\n3-Jaqueline Tavares\n4-Flavia Farias\n5-Celia Silva" +
+                "\n6-Silene Boratti\n7-Ana Bacca\n8-Monica Ferraz\n9-Sintia Santos\n10-Rafaela Booz" +
+                "\n11-Suzana dos Anjos\n12-Camila Gomes\n13-Julia Valentini";
+            idade = "16 à 25 Anos";
+            situacao = "Não Trabalha";
+            Console.WriteLine("Cadastro dos Mulheres que NÃO Trabalham!!! ");
+            Console.WriteLine("Nomes:" + nome + "\nSexo:" + sexo + "\nIdade: " + idade + "\nGenero: " + situacao + "\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaDiscurciva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            Jovem j = new Jovem();
+            j.HomensTrab();
+            j.HomensNaoTrab();
+            j.MulhesTrab();
+            j.MulhesNaoTrab();
+            Calculo();
+            Console.ReadLine();
+
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine(" Prova Discurciva da Matéria de Programação Orintada a Objeto!!!\n");
+            Console.WriteLine("Aluno: Leonardo Gattis Boratti");
+            Console.WriteLine("\n");
+        }
+        static void Calculo()
+        {
+            float HomensTrab, MulheresTrab, naoTrabMulheres, naoTrabHomens, Total;
+            float PerceHomensTrab, PerceHomensNao, PerceMulheresTrab, PerceMulheresNao;
+
+
+            HomensTrab = 19.1f;
+
+            MulheresTrab = 7.6f;
+
+            naoTrabMulheres = 13.05f;
+
+            naoTrabHomens = 8.8f;
+
+
+            Total = HomensTrab + MulheresTrab + naoTrabHomens + naoTrabMulheres;
+            PerceHomensTrab = HomensTrab / Total * 100;
+            PerceMulheresTrab = MulheresTrab / Total * 100;
+            PerceHomensNao = naoTrabHomens / Total * 100;
+            PerceMulheresNao = naoTrabMulheres / Total * 100;
+
+           
+            Console.WriteLine("A porcentagem de Homens que trabalham é " + PerceHomensTrab + "%");
+            Console.WriteLine("A porcentangem de Mulheres que trabalham é " + PerceMulheresTrab + "%");
+            Console.WriteLine("A porcentagem de Homens que Não trabalham é " + PerceHomensNao + "%");
+            Console.WriteLine("A porcentangem de Mulheres que Não trabalham é " + PerceMulheresNao + "%");
+        }
+
+    }
+}
